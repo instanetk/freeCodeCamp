bd011d618b5dd8efb10c7768f82a0b7ae668e6eb
David <davidq@gmail.com>
2019-04-24 18:45:01 -0400

    Study session April 24, 2019 | 4 pomodoros

--- bds.js
+++ bds.js
@@ -91,8 +91,389 @@ console.log(yourArray.length)
 
 let myArray = ["a", "b", "c", "d"];
 // change code below this line
-
+myArray[1] = "beautiful";
 //change code above this line
 
 console.warn("Line 54 // Basic Data Structures: Access an Array's Contents Using Bracket Notation")
 console.log(myArray);
+
+
+// Basic Data Structures: Add Items to an Array with push() and unshift()
+//
+// An array's length, like the data types it can contain, is not fixed.
+// Arrays can be defined with a length of any number of elements, and elements
+// can be added or removed over time; in other words, arrays are mutable.
+// In this challenge, we will look at two methods with which we can programmatically
+// modify an array: Array.push() and Array.unshift().
+//
+// Both methods take one or more elements as parameters and add those elements
+// to the array the method is being called on; the push() method adds elements
+// to the end of an array, and unshift() adds elements to the beginning.
+// Consider the following:
+//
+// let twentyThree = 'XXIII';
+// let romanNumerals = ['XXI', 'XXII'];
+//
+// romanNumerals.unshift('XIX', 'XX');
+// // now equals ['XIX', 'XX', 'XXI', 'XXII']
+//
+// romanNumerals.push(twentyThree);
+// // now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+//
+// Notice that we can also pass variables, which allows us even greater
+// flexibility in dynamically modifying our array's data.
+//
+//
+// We have defined a function, mixedNumbers, which we are passing an array
+// as an argument. Modify the function by using push() and unshift()
+// to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to the end
+// so that the returned array contains representations of the numbers 1-9 in order.
+
+function mixedNumbers(arr) {
+  // change code below this line
+arr.unshift('I', 2, 'three');
+arr.push(7, 'VIII', 9);
+  // change code above this line
+  return arr;
+}
+
+// do not change code below this line
+console.error("Line 101 // Basic Data Structures: Add Items to an Array with push() and unshift()")
+console.log(mixedNumbers(['IV', 5, 'six']));
+
+
+// Basic Data Structures: Remove Items from an Array with pop() and shift()
+//
+// Both push() and unshift() have corresponding methods that are nearly functional
+// opposites: pop() and shift(). As you may have guessed by now,
+// instead of adding, pop() removes an element from the end of an array,
+// while shift() removes an element from the beginning. The key difference
+// between pop() and shift() and their cousins push() and unshift(), is that
+// neither method takes parameters, and each only allows an array to be modified
+// by a single element at a time.
+//
+// Let's take a look:
+//
+// let greetings = ['whats up?', 'hello', 'see ya!'];
+//
+// greetings.pop();
+// // now equals ['whats up?', 'hello']
+//
+// greetings.shift();
+// // now equals ['hello']
+//
+// We can also return the value of the removed element with either method like this:
+//
+// let popped = greetings.pop();
+// // returns 'hello'
+// // greetings now equals []
+//
+// We have defined a function, popShift, which takes an array as an argument and
+// returns a new array. Modify the function, using pop() and shift(), to remove
+// the first and last elements of the argument array, and assign the removed
+// elements to their corresponding variables, so that the returned array contains
+// their values.
+
+function popShift(arr) {
+  let popped = arr.pop(); // change this line
+  let shifted = arr.shift();// change this line
+  return [shifted, popped];
+}
+
+// do not change code below this line
+console.warn("Line 145 // Basic Data Structures: Remove Items from an Array with pop() and shift()")
+console.log(popShift(['challenge', 'is', 'not', 'complete']));
+
+
+// Basic Data Structures: Remove Items Using splice()
+//
+// Ok, so we've learned how to remove elements from the beginning and end of arrays
+// using shift() and pop(), but what if we want to remove an element from somewhere
+// in the middle? Or remove more than one element at once? Well, that's where
+// splice() comes in. splice() allows us to do just that: remove any number of
+// consecutive elements from anywhere in an array.
+//
+// splice() can take up to 3 parameters, but for now, we'll focus on just the
+// first 2. The first two parameters of splice() are integers which represent
+// indexes, or positions, of the array that splice() is being called upon.
+// And remember, arrays are zero-indexed, so to indicate the first element of an
+// array, we would use 0. splice()'s first parameter represents the index on the
+// array from which to begin removing elements, while the second parameter
+// indicates the number of elements to delete. For example:
+//
+// let array = ['today', 'was', 'not', 'so', 'great'];
+//
+// array.splice(2, 2);
+// // remove 2 elements beginning with the 3rd element
+// // array now equals ['today', 'was', 'great']
+//
+// splice() not only modifies the array it's being called on, but it also returns
+// a new array containing the value of the removed elements:
+//
+// let array = ['I', 'am', 'feeling', 'really', 'happy'];
+//
+// let newArray = array.splice(3, 2);
+// // newArray equals ['really', 'happy']
+//
+// We've defined a function, sumOfTen, which takes an array as an argument and
+// returns the sum of that array's elements. Modify the function, using splice(),
+// so that it returns a value of 10.
+
+function sumOfTen(arr) {
+  // change code below this line
+  arr.splice(1, 2);
+  // change code above this line
+  return arr.reduce((a, b) => a + b);
+}
+
+// do not change code below this line
+console.error("Line 188 // Basic Data Structures: Remove Items Using splice()");
+console.log(sumOfTen([2, 5, 1, 5, 2, 1]));
+
+
+// Basic Data Structures: Add Items Using splice()
+//
+// Remember in the last challenge we mentioned that splice() can take up to
+// three parameters? Well, we can go one step further with splice() — in addition
+// to removing elements, we can use that third parameter, which represents one
+// or more elements, to add them as well. This can be incredibly useful for
+// quickly switching out an element, or a set of elements, for another.
+// For instance, let's say you're storing a color scheme for a set of DOM elements
+// in an array, and want to dynamically change a color based on some action:
+//
+// function colorChange(arr, index, newColor) {
+//   arr.splice(index, 1, newColor);
+//   return arr;
+// }
+//
+// let colorScheme = ['#878787', '#a08794', '#bb7e8c', '#c9b6be', '#d1becf'];
+//
+// colorScheme = colorChange(colorScheme, 2, '#332327');
+//
+// // we have removed '#bb7e8c' and added '#332327' in its place
+// // colorScheme now equals ['#878787', '#a08794', '#332327', '#c9b6be', '#d1becf']
+//
+// This function takes an array of hex values, an index at which to remove an
+// element, and the new color to replace the removed element with. The return value
+// is an array containing a newly modified color scheme! While this example is a
+// bit oversimplified, we can see the value that utilizing splice() to its maximum
+// potential can have.
+//
+// We have defined a function, htmlColorNames, which takes an array of HTML colors
+// as an argument. Modify the function using splice() to remove the first
+// two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their
+// respective places.
+
+function htmlColorNames(arr) {
+  // change code below this line
+  arr.splice(0, 2, 'DarkSalmon','BlanchedAlmond');
+  // change code above this line
+  return arr;
+}
+
+// do not change code below this line
+
+console.warn("Line 234 // Basic Data Structures: Add Items Using splice()");
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick']));
+
+
+// Basic Data Structures: Copy Array Items Using slice()
+//
+// The next method we will cover is slice(). slice(), rather than modifying an
+// array, copies, or extracts, a given number of elements to a new array,
+// leaving the array it is called upon untouched. slice() takes only 2 parameters —
+// the first is the index at which to begin extraction, and the second is the index
+// at which to stop extraction (extraction will occur up to, but not including the
+// element at this index). Consider this:
+//
+// let weatherConditions = ['rain', 'snow', 'sleet', 'hail', 'clear'];
+//
+// let todaysWeather = weatherConditions.slice(1, 3);
+// // todaysWeather equals ['snow', 'sleet'];
+// // weatherConditions still equals ['rain', 'snow', 'sleet', 'hail', 'clear']
+//
+// In effect, we have created a new array by extracting elements
+// from an existing array.
+//
+//
+// We have defined a function, forecast, that takes an array as an argument.
+// Modify the function using slice() to extract information from the argument array
+// and return a new array that contains the elements 'warm' and 'sunny'.
+
+function forecast(arr) {
+  // change code below this line
+
+  arr = arr.slice(2,4);
+
+  return arr;
+}
+
+// do not change code below this line
+console.error("Line 280 // Basic Data Structures: Copy Array Items Using slice()");
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+// Basic Data Structures: Copy an Array with the Spread Operator
+//
+// While slice() allows us to be selective about what elements of an array to copy,
+// among several other useful tasks, ES6's new spread operator allows us to easily
+// copy all of an array's elements, in order, with a simple and highly
+// readable syntax. The spread syntax simply looks like this: ...
+//
+// In practice, we can use the spread operator to copy an array like so:
+//
+// let thisArray = [true, true, undefined, false, null];
+// let thatArray = [...thisArray];
+// // thatArray equals [true, true, undefined, false, null]
+// // thisArray remains unchanged, and is identical to thatArray
+//
+// We have defined a function, copyMachine which takes arr (an array) and num
+// (a number) as arguments. The function is supposed to return a new array
+// made up of num copies of arr. We have done most of the work for you,
+// but it doesn't work quite right yet. Modify the function using spread syntax
+// so that it works correctly (hint: another method we have already covered
+// might come in handy here!).
+
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // change code below this line
+    newArr.push([...arr]);
+    // change code above this line
+    num--;
+  }
+  return newArr;
+}
+// copyMachine([true, false, true], 2) should return
+// [[true, false, true], [true, false, true]]
+
+// change code here to test different cases:
+
+console.warn("Line 316 // Basic Data Structures: Copy an Array with the Spread Operator");
+console.log(copyMachine([true, false, true], 2));
+
+
+// Basic Data Structures: Combine Arrays with the Spread Operator
+//
+// Another huge advantage of the spread operator, is the ability to combine arrays,
+// or to insert all the elements of one array into another, at any index.
+// With more traditional syntaxes, we can concatenate arrays, but this only allows us
+// to combine arrays at the end of one, and at the start of another.
+// Spread syntax makes the following operation extremely simple:
+//
+// let thisArray = ['sage', 'rosemary', 'parsley', 'thyme'];
+//
+// let thatArray = ['basil', 'cilantro', ...thisArray, 'coriander'];
+// // thatArray now equals ['basil', 'cilantro', 'sage', 'rosemary', 'parsley', 'thyme', 'coriander']
+//
+// Using spread syntax, we have just achieved an operation that would have been
+// more complex and more verbose had we used traditional methods.
+//
+// We have defined a function spreadOut that returns the variable sentence,
+// modify the function using the spread operator so that it returns
+// the array ['learning', 'to', 'code', 'is', 'fun'].
+
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ['learning', ...fragment, 'is', 'fun']; // change this line
+  return sentence;
+}
+
+// do not change code below this line
+
+console.error("Line 356 // Basic Data Structures: Combine Arrays with the Spread Operator");
+console.log(spreadOut());
+
+
+// Basic Data Structures: Check For The Presence of an Element With indexOf()
+//
+// Since arrays can be changed, or mutated, at any time, there's no guarantee about
+// where a particular piece of data will be on a given array, or if that element
+// even still exists. Luckily, JavaScript provides us with another built-in method,
+// indexOf(), that allows us to quickly and easily check for the presence of
+// an element on an array. indexOf() takes an element as a parameter,
+// and when called, it returns the position, or index, of that element, or -1
+// if the element does not exist on the array.
+//
+// For example:
+//
+// let fruits = ['apples', 'pears', 'oranges', 'peaches', 'pears'];
+//
+// fruits.indexOf('dates') // returns -1
+// fruits.indexOf('oranges') // returns 2
+// fruits.indexOf('pears') // returns 1, the first index at which the element exists
+//
+// indexOf() can be incredibly useful for quickly checking for the presence of
+// an element on an array. We have defined a function, quickCheck, that takes
+// an array and an element as arguments. Modify the function using indexOf()
+// so that it returns true if the passed element exists on the array,
+// and false if it does not.
+
+function quickCheck(arr, elem) {
+  // change code below this line
+
+  let test = arr.indexOf(elem);
+  if (test === -1){
+    return false;
+  } else {
+    return true;
+  }
+
+  // change code above this line
+}
+
+// change code here to test different cases:
+
+console.warn("Line 388 // Basic Data Structures: Check For The Presence of an Element With indexOf()");
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+
+// Basic Data Structures: Iterate Through All an Array's Items Using For Loops
+//
+// Sometimes when working with arrays, it is very handy to be able to iterate
+// through each item to find one or more elements that we might need, or to
+// manipulate an array based on which data items meet a certain set of criteria.
+// JavaScript offers several built in methods that each iterate over arrays in
+// slightly different ways to achieve different results (such as every(), forEach(),
+// map(), etc.), however the technique which is most flexible and offers us the
+// greatest amount of control is a simple for loop.
+//
+// Consider the following:
+//
+// function greaterThanTen(arr) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 10) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+//
+// greaterThanTen([2, 12, 8, 14, 80, 0, 1]);
+// // returns [12, 14, 80]
+//
+// Using a for loop, this function iterates through and accesses each element
+// of the array, and subjects it to a simple test that we have created.
+// In this way, we have easily and programmatically determined which data items
+// are greater than 10, and returned a new array containing those items.
+//
+// We have defined a function, filteredArray, which takes arr, a nested array,
+// and elem as arguments, and returns a new array. elem represents an element that
+// may or may not be present on one or more of the arrays nested within arr.
+// Modify the function, using a for loop, to return a filtered version of the
+// passed array such that any array nested within arr containing elem
+// has been removed.
+
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // change code below this line
+
+  // change code above this line
+  return newArr;
+}
+
+// change code here to test different cases:
+
+console.error("Line 431 // Basic Data Structures: Iterate Through All an Array's Items Using For Loops");
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
