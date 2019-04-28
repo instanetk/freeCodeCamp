eefbf7c914c019b76f2008281c87393b28976008
David <davidq@gmail.com>
2019-04-28 18:35:35 -0400

    Study session April 28, 2019 | 4 pomodoros

--- bds.js
+++ bds.js
@@ -468,12 +468,490 @@ console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
 function filteredArray(arr, elem) {
   let newArr = [];
   // change code below this line
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf(elem) === -1) { //Checks every parameter for the element and if is NOT there continues the code
+        newArr.push(arr[i]);
 
+    }
+  }
   // change code above this line
   return newArr;
 }
 
+// I spent significant time solving this problem. At first I was making use
+// of a nested for loop [i], [j]. I was also making a separate variable to
+// test the if statement with .indexOf instead of including it directly
+// within it.
+
 // change code here to test different cases:
 
 console.error("Line 431 // Basic Data Structures: Iterate Through All an Array's Items Using For Loops");
-console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([[9, 2, 5], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+
+
+// Basic Data Structures: Create complex multi-dimensional arrays
+//
+// Awesome! You have just learned a ton about arrays! This has been a fairly high
+// level overview, and there is plenty more to learn about working with arrays,
+// much of which you will see in later sections. But before moving on to looking
+// at Objects, lets take one more look, and see how arrays can become a bit more
+// complex than what we have seen in previous challenges.
+//
+// One of the most powerful features when thinking of arrays as data structures,
+// is that arrays can contain, or even be completely made up of other arrays.
+// We have seen arrays that contain arrays in previous challenges, but fairly
+// simple ones. However, arrays can contain an infinite depth of arrays that can
+// contain other arrays, each with their own arbitrary levels of depth, and so on.
+// In this way, an array can very quickly become very complex data structure,
+// known as a multi-dimensional, or nested array. Consider the following example:
+//
+// let nestedArray = [ // top, or first level - the outer most array
+//   ['deep'], // an array within an array, 2 levels of depth
+//   [
+//     ['deeper'], ['deeper'] // 2 arrays nested 3 levels deep
+//   ],
+//   [
+//     [
+//       ['deepest'], ['deepest'] // 2 arrays nested 4 levels deep
+//     ],
+//     [
+//       [
+//         ['deepest-est?'] // an array nested 5 levels deep
+//       ]
+//     ]
+//   ]
+// ];
+// While this example may seem convoluted, this level of complexity is not
+// unheard of, or even unusual, when dealing with large amounts of data.
+//
+// However, we can still very easily access the deepest levels of an array
+// this complex with bracket notation:
+//
+// console.log(nestedArray[2][1][0][0][0]);
+// // logs: deepest-est?
+//
+// And now that we know where that piece of data is, we can reset it if we need to:
+//
+// nestedArray[2][1][0][0][0] = 'deeper still';
+//
+// console.log(nestedArray[2][1][0][0][0]);
+// // now logs: deeper still
+//
+// We have defined a variable, myNestedArray, set equal to an array.
+// Modify myNestedArray, using any combination of strings, numbers,
+// and booleans for data elements, so that it has exactly five levels of depth
+// (remember, the outer-most array is level 1). Somewhere on the third level,
+// include the string 'deep', on the fourth level, include the string 'deeper',
+// and on the fifth level, include the string 'deepest'.
+
+  let myNestedArray = [                                   //level 1
+    ['unshift', false, 1, 2, 3, 'complex',                //level 2
+      ['loop', 'shift', 6, 7, 'deep',                     //level 3
+        ['concat', false, true, 'deeper',                 //level 4
+          ['mutate', 1327.98, 'splice', 'deepest', ]      //level 5
+        ]                                                 //close 4
+      ]                                                   //close 3
+    ]                                                     //close 2
+  ];                                                      //close 1
+
+
+
+console.warn("Line 492 // Basic Data Structures: Create complex multi-dimensional arrays");
+console.log(myNestedArray[0][6][5][4][3]);
+
+
+// Basic Data Structures: Add Key-Value Pairs to JavaScript Objects
+//
+// At their most basic, objects are just collections of key-value pairs,
+// or in other words, pieces of data mapped to unique identifiers that we call
+// properties or keys. Let's take a look at a very simple example:
+//
+// let FCC_User = {
+//   username: 'awesome_coder',
+//   followers: 572,
+//   points: 1741,
+//   completedProjects: 15
+// };
+//
+// The above code defines an object called FCC_User that has four properties,
+// each of which map to a specific value. If we wanted to know the number of
+// followers FCC_User has, we can access that property by writing:
+//
+// let userData = FCC_User.followers;
+// // userData equals 572
+//
+// This is called dot notation. Alternatively, we can also access the property
+// with brackets, like so:
+//
+// let userData = FCC_User['followers']
+// // userData equals 572
+//
+// Notice that with bracket notation, we enclosed followers in quotes.
+// This is because the brackets actually allow us to pass a variable in to be
+// evaluated as a property name (hint: keep this in mind for later!).
+// Had we passed followers in without the quotes, the JavaScript engine would
+// have attempted to evaluate it as a variable, and a ReferenceError:
+// followers is not defined would have been thrown.
+//
+//
+// Using the same syntax, we can also add new key-value pairs to objects.
+// We've created a foods object with three entries. Add three more entries:
+// bananas with a value of 13, grapes with a value of 35, and strawberries
+// with a value of 27.
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+// change code below this line
+foods['bananas'] = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+// change code above this line
+
+console.error("Line 563 // Basic Data Structures: Add Key-Value Pairs to JavaScript Objects");
+console.log(foods);
+
+
+// Basic Data Structures: Modify an Object Nested Within an Object
+//
+// Now let's take a look at a slightly more complex object. Object properties
+// can be nested to an arbitrary depth, and their values can be any type of data
+// supported by JavaScript, including arrays and even other objects.
+// Consider the following:
+//
+// let nestedObject = {
+//   id: 28802695164,
+//   date: 'December 31, 2016',
+//   data: {
+//     totalUsers: 99,
+//     online: 80,
+//     onlineStatus: {
+//       active: 67,
+//       away: 13
+//     }
+//   }
+// };
+// nestedObject has three unique keys: id, whose value is a number, date whose
+// value is a string, and data, whose value is an object which has yet another
+// object nested within it. While structures can quickly become complex,
+// we can still use the same notations to access the information we need.
+//
+//
+// Here we've defined an object, userActivity, which includes another object
+// nested within it. You can modify properties on this nested object in the
+// same way you modified properties in the last challenge. Set the value of
+// the online key to 45.
+
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+
+// change code below this line
+userActivity['data']['online'] = 45;
+// change code above this line
+
+console.warn("Line 618 // Basic Data Structures: Modify an Object Nested Within an Object");
+console.log(userActivity);
+
+
+// Basic Data Structures: Access Property Names with Bracket Notation
+//
+// In the first object challenge we mentioned the use of bracket notation as a way
+// to access property values using the evaluation of a variable. For instance,
+// imagine that our foods object is being used in a program for a supermarket
+// cash register. We have some function that sets the selectedFood and we want
+// to check our foods object for the presence of that food. This might look like:
+//
+// let selectedFood = getCurrentFood(scannedItem);
+// let inventory = foods[selectedFood];
+//
+// This code will evaluate the value stored in the selectedFood variable and
+// return the value of that key in the foods object, or undefined if
+// it is not present. Bracket notation is very useful because sometimes object
+// properties are not known before runtime or we need to access them in
+// a more dynamic way.
+//
+//
+// We've defined a function, checkInventory, which receives a scanned item as an
+// argument. Return the current value of the scannedItem key in the foods object.
+// You can assume that only valid keys will be provided as an argument
+// to checkInventory.
+
+let food = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+// do not change code above this line
+
+function checkInventory(scannedItem) {
+  // change code below this line
+  let result = food[scannedItem];
+  return result;
+}
+
+console.warn("Line 665 // Basic Data Structures: Access Property Names with Bracket Notation");
+// change code below this line to test different cases:
+console.log(checkInventory("apples"));
+
+
+// Basic Data Structures: Use the delete Keyword to Remove Object Properties
+//
+// Now you know what objects are and their basic features and advantages.
+// In short, they are key-value stores which provide a flexible, intuitive way
+// to structure data, and, they provide very fast lookup time.
+// Throughout the rest of these challenges, we will describe several common
+// operations you can perform on objects so you can become comfortable applying
+// these useful data structures in your programs.
+//
+// In earlier challenges, we have both added to and modified an object's key-value
+// pairs. Here we will see how we can remove a key-value pair from an object.
+//
+// Let's revisit our foods object example one last time. If we wanted to remove
+// the apples key, we can remove it by using the delete keyword like this:
+//
+// delete foods.apples;
+//
+// Use the delete keyword to remove the oranges, plums, and strawberries keys
+// from the foods object.
+
+let moreFoods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// change code below this line
+delete moreFoods.oranges;
+delete moreFoods.plums;
+delete moreFoods.strawberries;
+// change code above this line
+
+console.error("Line 709 // Basic Data Structures: Use the delete Keyword to Remove Object Properties");
+console.log(moreFoods);
+
+
+// Basic Data Structures: Check if an Object has a Property
+//
+// Now we can add, modify, and remove keys from objects. But what if we just
+// wanted to know if an object has a specific property? JavaScript provides us
+// with two different ways to do this. One uses the hasOwnProperty() method and
+// the other uses the in keyword. If we have an object users with a property
+// of Alan, we could check for its presence in either of the following ways:
+//
+// users.hasOwnProperty('Alan');
+// 'Alan' in users;
+// // both return true
+//
+// We've created an object, users, with some users in it and a function
+// isEveryoneHere, which we pass the users object as an argument.
+// Finish writing this function so that it returns true only if the users
+// object contains all four names, Alan, Jeff, Sarah, and Ryan, as keys, and
+// false otherwise.
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(obj) {
+  // change code below this line
+
+  if ('Alan','Jeff','Sarah','Ryan' in obj){
+    return true;
+  } else {
+    return false;
+  }
+
+  // change code above this line
+}
+
+console.error("Line 748 // ic Data Structures: Check if an Object has a Property");
+console.log(isEveryoneHere(users));
+
+
+// Basic Data Structures: Iterate Through the Keys of an Object with a for...in Statement
+//
+// Sometimes you may need to iterate through all the keys within an object.
+// This requires a specific syntax in JavaScript called a for...in statement.
+// For our users object, this could look like:
+//
+// for (let user in users) {
+//   console.log(user);
+// };
+//
+// // logs:
+// Alan
+// Jeff
+// Sarah
+// Ryan
+//
+// In this statement, we defined a variable user, and as you can see, this variable
+// was reset during each iteration to each of the object's keys as the statement
+// looped through the object, resulting in each user's name being
+// printed to the console.
+//
+// NOTE:
+// Objects do not maintain an ordering to stored keys like arrays do;
+// thus a keys position on an object, or the relative order in which it appears,
+// is irrelevant when referencing or accessing that key.
+//
+//
+// We've defined a function, countOnline; use a for...in statement within this
+// function to loop through the users in the users object and return the number
+// of users whose online property is set to true.
+
+let userList = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function countOnline(obj) {
+  // change code below this line
+  let result = 0;
+  for (let userList in obj){
+    if (obj[userList].online === true){
+       result++;
+    }
+  }
+  return result;
+  // change code above this line
+}
+
+console.warn("Line 801 // Basic Data Structures: Iterate Through the Keys of an Object with a for...in Statement");
+console.log(countOnline(userList));
+
+
+// Basic Data Structures: Generate an Array of All Object Keys with Object.keys()
+//
+// We can also generate an array which contains all the keys stored in an object
+// using the Object.keys() method and passing in an object as the argument.
+// This will return an array with strings representing each property in the object.
+// Again, there will be no specific order to the entries in the array.
+//
+//
+// Finish writing the getArrayOfUsers function so that it returns an array
+// containing all the properties in the object it receives as an argument.
+
+let users2 = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+  return Object.keys(obj);
+  // change code above this line
+}
+
+console.error("Line 867 // Basic Data Structures: Generate an Array of All Object Keys with Object.keys()");
+console.log(getArrayOfUsers(users2));
+
+
+// Basic Data Structures: Modify an Array Stored in an Object
+//
+// Now you've seen all the basic operations for JavaScript objects.
+// You can add, modify, and remove key-value pairs, check if keys exist,
+// and iterate over all the keys in an object. As you continue learning
+// JavaScript you will see even more versatile applications of objects.
+// Additionally, the optional Advanced Data Structures lessons later in
+// the curriculum also cover the ES6 Map and Set objects, both of which
+// are similar to ordinary objects but provide some additional features.
+// Now that you've learned the basics of arrays and objects, you're fully
+// prepared to begin tackling more complex problems using JavaScript!
+//
+//
+// Take a look at the object we've provided in the code editor.
+// The user object contains three keys. The data key contains five keys,
+// one of which contains an array of friends. From this, you can see how
+// flexible objects are as data structures. We've started writing a
+// function addFriend. Finish writing it so that it takes a user object
+// and adds the name of the friend argument to the array stored
+// in user.data.friends and returns that array.
+
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // change code below this line
+  let obj = userObj['data']['friends'];
+  obj.push(friend);
+  return obj;
+  // change code above this line
+}
+
+console.warn("Line 907 // Basic Data Structures: Modify an Array Stored in an Object");
+console.log(addFriend(user, 'Pete'));
