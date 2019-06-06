2d5772a595a8990265f249cbd59e70561486ddc6
David <davidq@gmail.com>
2019-06-06 17:17:00 -0400

    Study session June 6 | 1 hr 2 mins

--- algorithms.js
+++ algorithms.js
@@ -441,3 +441,34 @@ function getIndexToIns(arr, num) {
 console.warn("Basic Algorithm Scripting: Where do I Belong")
 console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
 //console.log(getIndexToIns([5, 3, 20, 3], 5));
+
+
+// Basic Algorithm Scripting: Mutations
+
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+
+  let arrX = arr[0].toLowerCase().split("").sort();
+  let arrX1 = arr[1].toLowerCase().split("").sort();
+
+  console.log(arrX +" | "+ arrX1);
+  
+  // It was necessary to check the length of the second array against the first one using indexOf.
+
+  for (let i = 0; i < arrX1.length; i++){
+      if (arrX.indexOf(arrX1[i]) < 0){
+        return false;
+      }
+}
+ return true;
+}
+
+console.error("Basic Algorithm Scripting: Mutations")
+console.log(mutation(["hello", "hey"]));
